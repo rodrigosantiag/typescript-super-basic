53f0dda36041002082ac5d42ca5bd410eaa5eea0
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-14 17:34:20 -0300

    Creating Negociacoes.ts model

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,12 @@
+class Negociacoes {
+    private _negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray() {
+        return this._negociacoes;
+    }
+
+}
