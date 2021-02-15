27b664811fca859f343ba0e55ea66e046f002993
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-15 19:15:16 -0300

    Creating template view for Negociacoes table

--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -1,12 +1,12 @@
 class Negociacoes {
     private _negociacoes: Array<Negociacao> = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
         this._negociacoes.push(negociacao);
     }
 
-    paraArray() {
-        return this._negociacoes;
+    paraArray(): Array<Negociacao> {
+        return [].concat(this._negociacoes);
     }
 
 }
